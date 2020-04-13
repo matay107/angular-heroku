ea9383184ec5f168455fcf9a8c500fcc0b3bc62a
praiwan107 <praiwan107@gmail.com>
2020-04-13 17:11:02 +0700

    first commit

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/angular-heroku'));
+app.use(express.static('./dist/angular-heroku-mon'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-heroku/'}),
+    res.sendFile('index.html', {root: 'dist/angular-heroku-mon/'}),
 );
 
 app.listen(process.env.PORT || 8080);
